07caf9dbfc763936225d2238ecf4b01b9f65295e
szymsad <szymsadzik@gmail.com>
2026-04-26 19:33:54 +0200

    doadanie dodawania paliwa ale stałe dane

--- fuel-tracker/src/App.tsx
+++ fuel-tracker/src/App.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
 import FuelList from "./components/FuelList"
 import { mockFuelData } from "./data/mockData"
+import type { FuelEntry } from "./types/FuelEntry"
 
 function App() {
+  const [entries, setEntries] = useState<FuelEntry[]>(mockFuelData)
+
   return (
     <div>
-      <h1>Fuel Tracker </h1>
-      <FuelList entries={mockFuelData} />
+      <h1>Fuel Tracker 🚗</h1>
+
+      <button onClick={() => {
+        const newEntry: FuelEntry = {
+          id: Date.now(),
+          date: "2026-04-25",
+          liters: 42,
+          pricePerLiter: 6.4,
+          mileage: 150800
+        }
+
+        setEntries([...entries, newEntry])
+      }}>
+        Dodaj tankowanie
+      </button>
+
+      <FuelList entries={entries} />
     </div>
   )
 }
